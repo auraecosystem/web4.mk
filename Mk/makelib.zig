fb65824e46656ffd0bcaae70292507ea5c6b097c
Seriki Walter Yakub <webapp4@outlook.com>
2026-07-07 20:23:25 +0100

    Create makelib.zig

---
+++ Mk/makelib.zig
@@ -0,0 +1,19 @@
+mk new fastapi my-api
+mk new flask my-app
+mk new node my-node
+mk new rust my-engine
+
+mk install docker
+mk install kubernetes
+mk install postgres
+
+mk search python
+mk search blockchain
+
+mk update
+
+mk doctor
+
+mk validate
+
+mk publish
